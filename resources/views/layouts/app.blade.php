ca77f42522754d0b7ccb9fd79dbb27d6d62eb0a1
Jiyo Collin Pagtananan <collin.113096@gmail.com>
2019-01-10 21:44:48 -0800

    View and Remove Requirement

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,7 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('js/app.js') }}"></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -50,16 +50,36 @@
                             @endif
                         @else
                             <li class="nav-item">
-                                <a class="nav-link" href="">Tuition Fee</a>
+                                <a class="nav-link" href="">Schedule</a>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="">Tuition</a>
+                                <div class="dropdown-menu">
+                                    <a href="" class="dropdown-item">Link1</a>
+                                       <a href="" class="dropdown-item">Link2</a>
+                                       <a href="" class="dropdown-item">Link3</a>
+                                       <a href="" class="dropdown-item">Link4</a>
+                                       <a href="" class="dropdown-item">Link5</a>
+                                       <a href="" class="dropdown-item">Link6</a>
+                                       <a href="" class="dropdown-item">Link7</a>
+                                       <a href="" class="dropdown-item">Link8</a>
+                                       <a href="" class="dropdown-item">Link9</a>
+                                       <a href="" class="dropdown-item">Link10</a>
+                                       <a href="" class="dropdown-item">Link11</a>
+                                       <a href="" class="dropdown-item">Link12</a>   
+                                </div>
                             </li>
                             <li class="nav-item">
                                 <a class="nav-link" href="">Charges <span class="badge badge-danger">5</span></a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="">Payment <span class="badge badge-success">10</span></a>
+                                <a class="nav-link" href="">Payment <span class="badge badge-danger">10</span></a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="">Notifications <span class="badge badge-primary">10</span></a>
+                                <a class="nav-link" href="">Balance <span class="badge badge-danger">10</span></a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="">Notifications <span class="badge badge-danger">10</span></a>
                             </li>
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
